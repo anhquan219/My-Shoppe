6723a1509dd5bbb9444dc465507f92c5afb36254
Anh Quan <quankull73@gmail.com>
2023-04-10 20:02:59 +0700

    Code page NotFound

--- src/useRouterElement.tsx
+++ src/useRouterElement.tsx
@@ -14,6 +14,7 @@ import UserLayout from './pages/User/UserLayout'
 import ChangePassword from './pages/User/pages/ChangePassword'
 import HistoryPurchase from './pages/User/pages/HistoryPurchase'
 import Profile from './pages/User/pages/Profile'
+import NotFound from './pages/NotFound'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
@@ -103,6 +104,14 @@ export default function useRouterElement() {
           ]
         }
       ]
+    },
+    {
+      path: '*',
+      element: (
+        <MainLayout>
+          <NotFound />
+        </MainLayout>
+      )
     }
   ])
   return routerElement
